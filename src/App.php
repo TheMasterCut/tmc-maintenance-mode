d3768d489d2e2800b58d52e3b94454dd615b1499
dualjack <jakubkuranda@gmail.com>
2018-04-16 23:19:08 +0200

    Removed: Init args.

--- src/App.php
+++ src/App.php
@@ -1,7 +1,7 @@
 <?php
 namespace tmc\mm\src;
 
-use shellpress\v1_1_9\ShellPress;
+use shellpress\v1_2_0\ShellPress;
 use tmc\mm\src\Components\Front;
 use tmc\mm\src\Components\Htaccess;
 use tmc\mm\src\Components\Settings;
