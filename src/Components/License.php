93fa35dc2a372891c3dae372ecbf912d89756ff2
dualjack <jakubkuranda@gmail.com>
2018-05-10 22:29:04 +0200

    Refactor: Component for SLM is ready.

--- src/Components/License.php
+++ src/Components/License.php
@@ -8,11 +8,8 @@ namespace tmc\mm\src\Components;
  */
 
 use shellpress\v1_2_1\src\Shared\Components\LicenseManagerSLM;
-use WP_Upgrader;
 
 class License extends LicenseManagerSLM {
-
-    const CHECK_LICENSE_CRON_JOB_NAME = 'rm_tmc_check_license';
 
     /**
      * Called on object creation.
@@ -21,22 +18,9 @@ class License extends LicenseManagerSLM {
      */
     protected function onSetUp() {
 
-        $this->addFormToAPF( 'tmc_mm_apf_page', 'license', 'tmc_mm_settings', 'tools' );
-
-        //  TODO - MOVE ALL THINGS FROM HERE TO LicenseManagerSLM.php
-
-        //  ----------------------------------------
-        //  Actions
-        //  ----------------------------------------
-
-        add_action( 'admin_notices',                            array( $this, '_a_displayLicenseNotification' ) );
-
-        $this::s()->event->addOnActivate(                       array( $this, '_a_registerCronJob' ) );
-        $this::s()->event->addOnDeactivate(                     array( $this, '_a_unregisterCronJob' ) );
-
-        add_action( $this::CHECK_LICENSE_CRON_JOB_NAME,         array( $this, '_a_doCheckLicenseCronCallback' ) );
-
-        add_action( 'upgrader_process_complete',                array( $this, '_a_handlePluginUpdateCronJobs' ), 10, 2 );
+        $this->registerAPFForm( 'tmc_mm_settings', 'tools', 'license' );
+        $this->registerNotices();
+        $this->registerAutomaticChecker();
 
     }
 
@@ -56,127 +40,6 @@ class License extends LicenseManagerSLM {
      */
     protected function onKeyDeactivationCallback() {
         // TODO: Implement onKeyDeactivationCallback() method.
-    }
-
-    //  ================================================================================
-    //  FILTERS
-    //  ================================================================================
-
-    /**
-     * Adds more intervals.
-     *
-     * @param array $schedules
-     *
-     * @return array
-     */
-    public function _f_modifyCronIntervals( $schedules ) {
-
-        $schedules['daily'] = array(
-            'interval'  =>  86400,
-            'display'   =>  __( 'Once a day' )
-        );
-
-        return $schedules;
-
-    }
-
-    //  ================================================================================
-    //  Actions
-    //  ================================================================================
-
-    /**
-     * Display error notice if passed license is inactive.
-     * Called on admin_notices hook.
-     *
-     * @return void
-     */
-    public function _a_displayLicenseNotification() {
-
-        if( $this->getKey() && ! $this->isActive() ){    //  If key is set and is inactive.
-
-            $adminPageUrl = admin_url( 'options-general.php?page=tmc_mm_settings&tab=tools' );
-
-            printf( '<div class="notice notice-error"><p>%1$s %2$s</p></div>',
-                __( 'Your license for The Real Maintenance Mode TMC is inactive.', 'rm_tmc' ),
-                sprintf( '<a href="%1$s">%2$s</a>', $adminPageUrl, __( 'Please remove license to dismiss this message.', 'rm_tmc' ) )
-            );
-
-        }
-
-    }
-
-    /**
-     * If plugin was updated, plugin activation hook is not called.
-     * This method fixes it!
-     *
-     * Called on upgrader_process_complete.
-     *
-     * @param WP_Upgrader $upgrader
-     * @param array $options
-     *
-     * @return void
-     */
-    public function _a_handlePluginUpdateCronJobs( $upgrader, $options ) {
-
-        if( $options['action'] == 'update' && $options['type'] == 'plugin' ){   //  Updating plugin.
-
-            $pluginName = plugin_basename( $this::s()->getMainPluginFile() );
-
-            if( is_array( $options['plugins'] ) && in_array( $pluginName, $options['plugins'] ) ){  //  Updating THIS plugin
-
-                add_action( 'init', array( $this, '_a_registerCronJob' ) );
-
-            }
-
-        }
-
-    }
-
-    /**
-     * Called on plugin activation.
-     *
-     * @return void
-     */
-    public function _a_registerCronJob() {
-
-        if( ! wp_next_scheduled( $this::CHECK_LICENSE_CRON_JOB_NAME ) ){
-
-            wp_schedule_event( time(), 'daily', $this::CHECK_LICENSE_CRON_JOB_NAME );
-
-            $this::s()->log->info( 'Added cron job: ' . $this::CHECK_LICENSE_CRON_JOB_NAME );
-
-        }
-
-    }
-
-    /**
-     * Called on plugin deactivation.
-     *
-     * @return void
-     */
-    public function _a_unregisterCronJob() {
-
-        wp_clear_scheduled_hook( $this::CHECK_LICENSE_CRON_JOB_NAME );
-
-    }
-
-    /**
-     * Does remote requests and checks given license key.
-     *
-     * @return void
-     */
-    public function _a_doCheckLicenseCronCallback() {
-
-        $this::s()->log->info( 'START LICENSE CHECK CRON JOB' );
-
-        $key = $this->getKey();
-
-        if( $key ){
-            $this->performRemoteKeyUpdate();
-        }
-
-        $this::s()->options->flush();
-
     }
 
 }
